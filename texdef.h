c357281dffaa28f0940260bff64ca7d0b73de82c
akonya <akonya1@gmail.com>
2013-10-10 14:29:06 -0400

    begin mesh initialization and data host->dev

---
+++ texdef.h
@@ -0,0 +1,15 @@
+#ifndef __TEXDEF_H__
+#define __TEXDEF_H__
+
+#include "mainhead.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <stdio.h>
+
+//texture refference deffinitions
+texture<float, 2, cudaReadModeElementType> texRef_P;
+
+
+
+#endif //__TEXDEF_H__
